b764819924a7957434942d9de2ee2bc6be98f16e
Sellith <leanevanbree@gmail.com>
2026-07-08 12:16:19 +0200

    S1: Ex03 done

---
+++ Symfony_1/ex03/array2hash_sorted.php
@@ -0,0 +1,12 @@
+<?php
+function array2hash(array $arrays):array {
+    $hash = [];
+    foreach($arrays as $array) {
+        $name  = $array[0];
+        $age = $array[1];
+        $hash[$name] = $age;
+    }
+    krsort($hash);
+    return $hash;
+}
+?>
